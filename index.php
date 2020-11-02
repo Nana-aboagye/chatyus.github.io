2de423764400e6772a998b8b987153875d0b4018
Nana Aboagye <aboagyeprince91@gmail.com>
2020-11-02 15:52:30 +0000

    newly added

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "layouts/header.php"; ?>
+<div class="container">
+
+<center><h2 style="color:white; margin-top:20%">Welcome to simple chat system.</h2></center>
+</div>
+
+</body>
+<link rel="icon" href="images/logo.png" type="image/png" sizes="16x16">
+</html>
